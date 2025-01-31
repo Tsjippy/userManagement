8b73276aa171f60c55d9322a2a9c5c3cb2286434
Tsjippy <ewaldha@hotmail.com>
2025-01-31 13:39:05 +0100

    changed: extra js hook

--- php/addExtraJs.php
+++ php/addExtraJs.php
@@ -5,10 +5,10 @@ use SIM;
 /* HELPER FUNCTIONS */
 //add special js to the dynamic form js
 add_filter('sim_form_extra_js', __NAMESPACE__.'\addJs', 10, 3);
-function addJs($js, $formName, $minimized){
-	$path	= plugin_dir_path( __DIR__)."js/$formName.min.js";
+function addJs($js, $object, $minimized){
+	$path	= plugin_dir_path( __DIR__)."js/$object->formName.min.js";
 	if(!$minimized || !file_exists($path)){
-		$path	= plugin_dir_path( __DIR__)."js/$formName.js";
+		$path	= plugin_dir_path( __DIR__)."js/$object->formName.js";
 	}
 
 	if(file_exists($path)){
